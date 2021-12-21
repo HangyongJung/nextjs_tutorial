5a5324a7794624f8d0fc8bd84a2f9e0c7ebd74be
gkddyd <gkddyd@naver.com>
2021-12-21 20:19:48 +0900

    fix : lec01 반영

--- src/component/Top.js
+++ src/component/Top.js
@@ -1,3 +1,20 @@
+import { Header } from 'semantic-ui-react';
+import Gnb from './Gnb';
+
 export default function Top() {
-    return <div>TOP</div>
-};
+    return (
+        <div>
+            <div style={{ display: 'flex', paddingTop: 20 }}>
+                <div style={{ flex: '100px 0 0' }}>
+                    <img
+                        src="/vercel.svg"
+                        alt="logo"
+                        style={{ dsplay: 'block', width: 200 }}
+                    />
+                </div>
+                <Header as="h1">코딩초보</Header>
+            </div>
+            <Gnb />
+        </div>
+    );
+}

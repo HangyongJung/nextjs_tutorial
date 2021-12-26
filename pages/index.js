bd7a483ba94628af8ebb5fc6277a3c5c79714a6d
gkddyd <gkddyd@naver.com>
2021-12-26 20:55:10 +0900

    fix : itemList add

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,52 @@
+import Axios from 'axios';
 import Head from 'next/head';
 import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import { Dimmer, Divider, Header, Item, Loader } from 'semantic-ui-react';
+import ItemList from '../src/component/itemList';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
+    const [list, setList] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const API_URL = "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
+    function getData() {
+        Axios.get(API_URL).then((res) => {
+            console.log(res.data);
+            setList(res.data);
+            setIsLoading(false);
+        });
+    }
+
+    useEffect(() => {
+        getData();
+    }, []);
+
     return (
         <div>
             <Head>
                 <title>코딩초보</title>
             </Head>
-            Create-next-app
+            {isLoading && (
+                <div style={{ padding: "300px 0" }}>
+                    <Dimmer active>
+                        <Loader inline="centered" active>
+                            Loading..
+                        </Loader>
+                    </Dimmer>
+                </div>
+            )}
+            {!isLoading && (
+                <>
+                    <Header as="h3" style={{ paddingTop: 40 }}>베스트 상품</Header>
+                    <Divider />
+                    <ItemList list={list.slice(0, 9)} />
+
+                    <Header as="h3" style={{ paddingTop: 40 }}>신상품</Header>
+                    <Divider />
+                    <ItemList list={list.slice(10,)} />
+                </>
+            )}
         </div>
-    );
+    )
 }
